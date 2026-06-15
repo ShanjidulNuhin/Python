b6faf539f25cc24a2c891f082c2d3e334d4a5f5c
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-15 22:54:58 +0600

    testing

--- task3.py
+++ task3.py
@@ -8,10 +8,13 @@
 # balance=53.44
 # name = "John Doe"
 # print("Hello %s. Your current balance is %f."%(name,balance))
-age = 23
-if age >= 18:print(f"hello! i see your age is {age}")
-
-name=["john","rick"]
-inname=input("enter your name")
-if inname in name:print(f"Your name is {inname}")
-else:print(f"Your name is not found in the list")
+# age = 23
+# if age >= 18:print(f"hello! i see your age is {age}")
+#
+# name=["john","rick"]
+# inname=input("enter your name")
+# if inname in name:print(f"Your name is {inname}")
+# else:print(f"Your name is not found in the list")
+score =100
+print (f"the original number is {score/3}")
+print (f" the proper int number is {score//3}")
