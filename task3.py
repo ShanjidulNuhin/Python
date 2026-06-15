95bf37de0cdce7ca03a473035d1c343ebd4cad4b
MD. Shanjidul Islam <shanjidshanjidulislam1234@gmail.com>
2026-06-15 07:07:28 +0600

    task 3

--- task3.py
+++ task3.py
@@ -1,10 +1,17 @@
-text ="Hello\nWold"
-print(text)
-print(repr(text))
+# text ="Hello\nWold"
+# print(text)
+# print(repr(text))
+#
+# # You will need to write a format string which prints out the data using the following syntax:
+# # Hello John Doe. Your current balance is $53.44.
+#
+# balance=53.44
+# name = "John Doe"
+# print("Hello %s. Your current balance is %f."%(name,balance))
+age = 23
+if age >= 18:print(f"hello! i see your age is {age}")
 
-# You will need to write a format string which prints out the data using the following syntax:
-# Hello John Doe. Your current balance is $53.44.
-
-balance=53.44
-name = "John Doe"
-print("Hello %s. Your current balance is %f."%(name,balance))
+name=["john","rick"]
+inname=input("enter your name")
+if inname in name:print(f"Your name is {inname}")
+else:print(f"Your name is not found in the list")
